37d7ec6cab524f5ad05d95a7fdbd406e57ef4f4b
Babalwa01 <babalwambolekwa93@gmail.com>
2021-04-05 23:08:06 +0200

    mzamomhle project info added

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -10,6 +10,50 @@ const aboutScrollBar3 = document.querySelector('#third-bar');
 const aboutScrollBar4 = document.querySelector('#fourth-bar');
 const navInstruction = document.querySelector('.navigation-instruction');
 
+// const sliderImages = document.querySelectorAll(".mzamomhle-slide");
+// const arrowLeft = document.querySelector("#arrow-left");
+// const arrowRight = document.querySelector("#arrow-right");
+// let current = 0;
+
+// function reset() {
+//     for(let i = 0; i < sliderImages.length; i++) {
+//         sliderImages[i].style.display = "none";
+//     }
+// }
+
+// function startSlide() {
+//     reset();
+//     sliderImages[0].style.display = "block";
+// }
+
+// function slideLeft() {
+//     reset();
+//     sliderImages[current - 1].style.display = "block";
+//     current--;
+// }
+
+// function slideRight() {
+//     reset();
+//     sliderImages[current + 1].style.display = "block";
+//     current++;
+// }
+
+// arrowLeft.addEventListener("click", function() {
+//     if(current === 0) {
+//         current = sliderImages.length;
+//     }
+//     slideLeft();
+// });
+
+// arrowRight.addEventListener("click", function() {
+//     if(current === sliderImages.length - 1) {
+//         current = -1;
+//     }
+//     slideRight();
+// });
+
+// startSlide();
+
 //set initial state of menu
 let showMenu = false;
 
